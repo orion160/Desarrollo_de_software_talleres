6fdb36583a7a703711f716d53de313f8017462fb
Isaac <idbl64@outlook.com>
2021-10-02 20:45:58 -0500

    Taller 1

---
+++ tallerTypescript/dataCourses.ts
@@ -0,0 +1,7 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+    new Course("Desarrollo SW en equipo", "Rubby Casallas", 3),
+    new Course("Lenguajes y maquinas", "Nicolas Cardoso", 3),
+    new Course("Sistemas transaccionales", "German Bravo", 3),
+]
---
+++ tallerTypescript/scripts/dataCourses.js
@@ -0,0 +1,6 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo SW en equipo", "Rubby Casallas", 3),
+    new Course("Lenguajes y maquinas", "Nicolas Cardoso", 3),
+    new Course("Sistemas transaccionales", "German Bravo", 3),
+];
